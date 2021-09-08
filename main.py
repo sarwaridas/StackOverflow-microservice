372fcb3a26959cd4dcb56d93312215996259f6a1
EC2 Default User <ec2-user@ip-172-31-3-198.us-east-2.compute.internal>
2021-09-08 12:37:11 +0000

    changes in main file

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/add/{num1}/{num2}")
-async def add(num1: int, num2: int):
-    """Add two numbers together"""
+#@app.get("/add/{num1}/{num2}")
+#async def add(num1: int, num2: int):
+ #   """Add two numbers together"""
 
-    total = num1 + num2
-    return {"total": total}
+  #  total = num1 + num2
+   # return {"total": total}
 
 @app.get("/answer/str1")
 async def answer(str1: str):
